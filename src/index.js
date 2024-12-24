b38820a9bc82a1178c578d2364e6e21614c7f39a
Florian <floriangeorges1992@gmail.com>
2024-12-24 10:36:27 +0100

    add ctx canvas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+import "./style.scss";
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.moveTo(100, 100);
+ctx.lineTo(400, 400);
+ctx.arcTo(500, 500, 400, 600, 50);
+ctx.lineTo(300, 700);
+
+ctx.lineWidth = 5;
+ctx.strokeStyle = "red";
+ctx.lineCap = "round";
+ctx.lineJoin = "round";
+ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
+ctx.closePath();
+ctx.fill();
+ctx.stroke();
