efe4b854e5363abbdd31b8f7140c98cd4cfb803c
Florian <floriangeorges1992@gmail.com>
2024-12-24 11:13:01 +0100

    upddate

--- src/index.js
+++ src/index.js
@@ -53,3 +53,32 @@ ctx.beginPath();
 ctx.fillStyle = "#e17055";
 ctx.rect(250, 500, 200, 100);
 ctx.fill();
+ctx.closePath();
+
+// Nous commençons un nouveau parcours
+ctx.beginPath();
+// Les deux premiers arguments sont les coordonnées du centre
+// du canvas (500/2, 500/2) pour le centre du cercle. Le troisième argument
+// est le rayon. Voir plus bas pour les angles.
+ctx.arc(250, 250, 240, (1 / 4) * Math.PI, (7 / 4) * Math.PI);
+// Nous traçons une première ligne entre le point de fin du tracé du cercle
+// et le centre du cercle :
+ctx.lineTo(250, 250);
+// Nous utilisons closePath pour fermer la figure et donc le corps de Pacman :
+ctx.closePath();
+
+// Nous remplissons en jaune la figure :
+ctx.fillStyle = "yellow";
+ctx.fill();
+// Pour le tracé de l’arc de cercle et des lignes on le fait en noir :
+ctx.strokeStyle = "black";
+ctx.stroke();
+
+// Ensuite on passe à l’oeil, on commence donc un nouveau parcours :
+ctx.beginPath();
+// On part de la moitié du canvas pour l’axe x et la moitié du rayon pour
+// l’axe y pour que le cercle soit bien centré :
+ctx.arc(250, 120, 20, 0, 2 * Math.PI);
+// On remplit le cercle de noir :
+ctx.fillStyle = "black";
+ctx.fill();
